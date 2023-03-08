0fd93261cb44c26c35a3bfba67adcaa43a7b05a5
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2023-03-08 19:19:14 +0700

    Create `kelas-1-2` page

---
+++ src/pages/outing/kelas-1-2.tsx
@@ -0,0 +1,178 @@
+export interface OutingKelas12Props {}
+
+const OutingKelas12 = ({}: OutingKelas12Props) => {
+    return (
+        <>
+            <header className="Hero">
+                <video
+                    autoPlay
+                    muted
+                    loop
+                    playsInline
+                    className="Hero__video"
+                    poster="http://sdngisikdrono01.dikdas.semarangkota.go.id/uploads/gallery/media/kelas1_walk.png"
+                >
+                    <source src="https://firebasestorage.googleapis.com/v0/b/sdngisa-18335.appspot.com/o/outing%2Fkelas1_walk.MOV?alt=media&token=eafd579d-ded6-4359-8fe2-eb15fdab4f75" />
+                </video>
+                <img
+                    src="https://firebasestorage.googleapis.com/v0/b/sdngisa-18335.appspot.com/o/logo_gisa.png?alt=media&token=28bfbafd-f25a-424f-add6-d65630965531"
+                    className="Hero__logo"
+                    title="Logo GISA"
+                    alt="Logo GISA"
+                />
+
+                <div className="Hero__heading">
+                    <h1>
+                        SDN <span className="Hero__heading--purple">GISA</span> Goes
+                        to <span className="Hero__heading--green">Maerakaca!</span>
+                    </h1>
+                    <h2>SDN Gisikdrono 01</h2>
+                    <a className="btn" href="#opening">
+                        Baca Lebih Lanjut
+                    </a>
+                </div>
+            </header>
+
+            <section className="section" id="opening">
+                <p>Halo warga GISA!</p>
+                <img
+                    className="img"
+                    alt="Photo Warga GISA"
+                    title="Photo Warga GISA"
+                    src="http://sdngisikdrono01.dikdas.semarangkota.go.id/uploads/gallery/media/kelas12_intro.png"
+                />
+                <p>
+                    Semarang tiba-tiba panas <br />
+                    Segar pasti minum kelapa muda <br />
+                    Ayo teman jangan malas <br />
+                    Hari ini kita ke Maerakaca!
+                </p>
+                <p>
+                    Pagi hari Selasa 7 Maret 2023, terlihat gejala-gejala bahwa hari
+                    ini merupakan hari yang berbeda. Para murid kelas 1 & 2 bergegas
+                    memasuki sekolah mengenakan pakaian olahraga merah muda merona
+                    dan kumpulan orang tua menunggu di depan sekolah dengan membawa
+                    bekal-bekal yang lezat. Ketidaksabaran pekat terngiang dalam
+                    atmosfir apel karena para siswa dan orang tua tahu bahwa hari ini
+                    hari yang spesial: class outing ke Grand Maerakaca.
+                </p>
+            </section>
+
+            <section className="section--right">
+                <p>
+                    Seketika selesai apel pagi, para murid kelas 1 & 2 berbaris dan
+                    bergegas dengan rapi keluar dari sekolah. Mereka disambut oleh
+                    para pendamping yang menemani mereka berangkat dari sekolah.
+                    Murid kelas 1 menaiki kereta mini dan ditemani oleh lagu ceria
+                    seiring jalan; murid kelas 2 menyusul menggunakan angkot.
+                </p>
+                <img
+                    className="img"
+                    alt="Photo Warga GISA"
+                    title="Photo Warga GISA"
+                    src="http://sdngisikdrono01.dikdas.semarangkota.go.id/uploads/gallery/media/kelas1_kereta.png"
+                />
+            </section>
+
+            <section className="section--left">
+                <p>
+                    Sesampainya disana, para siswa bergegas turun dari kendaraan
+                    mereka dan berbaris rapi di depan pintu masuk. Mereka disambut
+                    oleh pemandu wisata yang memulai kegiatan dengan jalan-jalan
+                    sembari mengenali beberapa daerah mini dari depan Maerakaca
+                    menuju danaunya.
+                </p>
+                <img
+                    className="img"
+                    alt="Photo Warga GISA"
+                    title="Photo Warga GISA"
+                    src="http://sdngisikdrono01.dikdas.semarangkota.go.id/uploads/gallery/media/kelas1_jalan2.png"
+                />
+            </section>
+
+            <section className="section--right">
+                <p>
+                    Para murid rehat dalam Anjungan Kabupaten Batang. Mereka dipandu
+                    untuk duduk dan menyaksikan kegiatan-kegiatan selanjutnya. Para
+                    pemandu memperkenalkan lebih lanjut mengenai Grand Maerakaca dan
+                    mengajak semuanya untuk menyaksikan 3 eksperimen. Eksperimen
+                    pertama menguji kebenaran Laut Mati menggunakan telur yang
+                    mengapung akibat kadar garam dalam air, eksperimen selanjutnya
+                    mengisi balon menggunakan karbon dioksida dari reaksi cuka &
+                    baking powder; dan eksperimen terakhir meluapkan soda menggunakan
+                    permen mentos.
+                </p>
+                <img
+                    className="img"
+                    alt="Photo Warga GISA"
+                    title="Photo Warga GISA"
+                    src="http://sdngisikdrono01.dikdas.semarangkota.go.id/uploads/gallery/media/kelas12_eksperimen.png"
+                />
+            </section>
+
+            <section className="section--left">
+                <p>
+                    Setelah bereksperimen, pemandu memberikan setiap anak paket yang
+                    berisi patung polos, 2 cat, dan 2 cotton bud. Para siswa
+                    diberikan waktu untuk mengecat patung mereka sekreatif keinginan
+                    mereka. Ada yang mengecat dengan rapi dan ada yang menggunakan
+                    jari. Pada akhirnya, para murid bersenang-senang dan menyimpan
+                    karya mereka dengan bangga.
+                </p>
+                <img
+                    className="img"
+                    alt="Photo Warga GISA"
+                    title="Photo Warga GISA"
+                    src="http://sdngisikdrono01.dikdas.semarangkota.go.id/uploads/gallery/media/kelas12_lukis.png"
+                />
+            </section>
+
+            <section className="section--right">
+                <p>
+                    Seselesainya membersihkan diri, para murid kelas 1 menaiki kereta
+                    mini dimana mereka diperkenalkan pada daerah-daerah mini baru.
+                    Ketika berhenti di Kabupatan Cilacap, mereka bergegas memainkan
+                    alat-alat edukatif yang tersedia disana. Setelah puas, mereka
+                    pulang kembali ke Anjungan dan bergantian dengan kelas 2 untuk
+                    menaiki kapal. Sambil berlaut, pemandu menunjukkan daerah-daerah
+                    baru dan para murid bersorak ketika menyaksikan ikan bandeng
+                    melompat.
+                </p>
+                <img
+                    className="img"
+                    alt="Photo Warga GISA"
+                    title="Photo Warga GISA"
+                    src="http://sdngisikdrono01.dikdas.semarangkota.go.id/uploads/gallery/media/kelas12_kapal.png"
+                />
+            </section>
+
+            <section className="section">
+                <p>
+                    Berkegiatan seharian tentu menghabiskan perut; atas dukungan dari
+                    para orang tua murid, tidak ada perut yang lapar karena bekal
+                    lezat dari mereka dan rangkaian acara berjalan lancar dari mulai
+                    hingga selesai. Para murid kembali ke sekolah menggunakan
+                    kendaraan yang sama dan setelah berkumpul sebentar di kelas,
+                    mereka bergegas pulang dengan senyuman di muka mereka
+                </p>
+                <a
+                    href="https://www.youtube.com/watch?v=c9yHisNVEQE"
+                    target="_blank"
+                >
+                    <video
+                        autoPlay
+                        className="img"
+                        loop
+                        muted
+                        playsInline
+                        poster="http://sdngisikdrono01.dikdas.semarangkota.go.id/uploads/gallery/media/kelas12_outing_video_poster.png"
+                    >
+                        <source src="https://firebasestorage.googleapis.com/v0/b/sdngisa-18335.appspot.com/o/outing%2Fkelas12_vlog_teaser.mp4?alt=media&token=293d4d1d-b41b-4f8e-8e75-dd1396b88d0d" />
+                    </video>
+                </a>
+            </section>
+        </>
+    );
+};
+
+export default OutingKelas12;
