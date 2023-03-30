62836f50ea59c4aa6763b9f00a69b6862e798afd
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2023-03-30 20:39:56 +0700

    Add old home code

---
+++ src/pages/home/old.tsx
@@ -0,0 +1,146 @@
+export interface OldHomeProps {}
+
+const OldHome = ({}: OldHomeProps) => {
+    return (
+        <>
+            {/* <div style="background-image: url('http://sdnpudakpayung01.dikdas.semarangkota.go.id/uploads/gallery/media/background-wallpaper-batik-ok.jpg'); padding-top: 15px; margin-bottom: 45px;"><!-- BACKGROUND HALAMAN -->
+<div class="row" style="margin-left: 15px; margin-right:15px; margin-top: 15px;">
+<p><img src="http://sdngisikdrono01.dikdas.semarangkota.go.id/uploads/gallery/media/IMG-20210522-WA0018_11zon.jpeg" style="float:left; height:333px; margin:5px 25px 5px 5px; width:250px" /> <span style="font-size:28px"><strong><span style="font-family:georgia,serif">SAMBUTAN KEPALA SEKOLAH</span></strong></span></p>
+
+<hr />
+<p style="text-align:justify"><span style="font-size:14px"><span style="font-size:16px">Assalamualaikum wr.wb.</span></span></p>
+
+<p style="text-align:justify"><span style="font-size:16px">Om swastyastu namo buddhaya.</span></p>
+
+<p style="text-align:justify"><span style="font-size:16px">Salam sejahtera bagi kita semua</span></p>
+
+<p style="text-align:justify">&nbsp;</p>
+
+<p style="margin-left:200px; text-align:justify"><span style="font-size:16px">Bapak/Ibu/ Saudara/i, serta Anak-anak generasi penerus yang saya banggakan. Puji dan syukur kita panjatkan kehadirat Tuhan YME yang telah melimpahkan berkatNya bagi kita tanpa batas.</span></p>
+
+<p style="margin-left:200px; text-align:justify"><span style="font-size:16px">Saya sampaikan terima kasih kepada Kepala Dinas Pendidikan Kota Semarang, Bapak Gunawan Saptogiri, S.H., M.M. beserta jajaran yang telah memfasilitasi SDN Gisikdrono 01 untuk dapat memiliki <em>website</em> sekolah.</span></p>
+
+<p style="text-align:justify"><span style="font-size:16px">Sungguh ada harapan besar bagi keluarga besar SDN Gisikdrono 01 Semarang, di masa mendatang untuk semakin terpacu dalam meningkatkan pelayanan kepada masyarakat di bidang pendidikan, khususnya bagi peserta didik SDN Gisikdrono 01. Dengan dimilikinya <em>website</em> sekolah, saya juga berharap akan semakin menjalin komunikasi yang harmonis dengan masyarakat, mempermudah <em>stakeholder</em> dalam mengakses segala informasi yang ada di sekolah, serta memupuk kompetensi warga sekolah dalam mewujudkan visi-misi sekolah.</span></p>
+
+<p style="text-align:justify"><span style="font-size:16px">Semoga kita semua senantiasa dalam lindungan dan bimbingan Tuhan.</span></p>
+
+<p style="text-align:justify"><span style="font-size:16px">Wassalamualaikum wr.wb.</span></p>
+&nbsp;
+
+<p>&nbsp;</p>
+</div>
+<!--./container-->
+
+<div class="row" style="margin-left: 15px; margin-right:15px;">
+<p>&nbsp;</p>
+
+<div>
+<h3 style="color:rgb(170, 170, 170); font-style:italic; text-align:center"><strong><span style="font-family:georgia,serif"><span dir="ltr"><span style="font-size:36px"><span style="color:rgb(255, 0, 0)">Sejarah Singkat SDN Gisikdorono 01</span></span></span></span></strong></h3>
+
+<h3 style="color:rgb(170, 170, 170); font-style:italic">&nbsp;</h3>
+
+<p style="text-align:center"><img src="http://sdngisikdrono01.dikdas.semarangkota.go.id/uploads/gallery/media/sdngisikdrono01 (2).jpg" style="height:600px; width:800px" /></p>
+&nbsp;
+
+<p style="text-align:justify"><span style="font-size:16px">Secara administrasi Sekolah Dasar (SD) Negeri Gisikdrono 01 beralamat di Jalan Puspogiwang IV No.8, Kelurahan Gisikdrono, Kecamatan Semarang Barat, Kota Semarang, Provinsi Jawa Tengah. </span></p>
+
+<p style="text-align:justify"><span style="font-size:16px">SD Negeri Gisikdrono 01 memiliki luas lahan 2.850 M², Dengan batas-batas :</span></p>
+
+<ul>
+	<li>
+	<p style="text-align:justify"><span style="font-size:16px"><cite>Utara : Jl. Taman Puspogiwang 01;</cite></span></p>
+	</li>
+	<li>
+	<p style="text-align:justify"><span style="font-size:16px"><cite>Timur : Permukiman;</cite> </span></p>
+	</li>
+	<li>
+	<p style="text-align:justify"><span style="font-size:16px"><cite>Selatan : Masjid An-Nur</cite> </span></p>
+	</li>
+	<li>
+	<p style="text-align:justify"><span style="font-size:16px"><cite>Barat : Permukiman</cite></span></p>
+	</li>
+</ul>
+
+<p style="text-align:justify"><span style="font-size:16px">Jarak tempuh ± 5&nbsp;menit dr kota Propinsi, SD Negeri Gisikdrono terdiri dari 1 (Satu) Bangunan utama, &nbsp;dgn rincian 6 (enam) ruangan kelas, 1 (satu) ruangan Guru, 1 (satu) ruang Kepala Sekolah, 1 (satu) ruangan Perpustakaan 1 (Satu) Ruang UKS dan 1 (Satu) Ruang Agama. <a name="_GoBack"></a> </span></p>
+</div>
+
+<p>&nbsp;</p>
+</div>
+
+<div class="col-md-12 col-sm-12">
+<div class="row">
+<div class="col-md-8 col-sm-12 col-md-offset-2 text-center">
+<hr />
+<h3><strong><em><span style="font-size:36px"><span style="font-family:georgia,serif"><span style="color:rgb(255, 0, 0)">GALERI FOTO</span> </span> </span> </em> </strong></h3>
+
+<h1><strong>SERBA SERBI KEGIATAN SD NEGERI GISIKDRONO 01</strong></h1>
+
+<div class="divider">&nbsp;</div>
+</div>
+<!--./col-md-8-->
+
+<div class="owl-carousel testimonial-carousel"><!-- ITEM GALERI -->
+<div class="col-md-12 col-sm-12">
+<div class="testimonial">
+<div class="testi_description">
+<h4><img src="http://sdngisikdrono01.dikdas.semarangkota.go.id/uploads/gallery/media/IMG-20210525-WA0051.jpg" style="height:350px; width:500px" /></h4>
+</div>
+
+<h2><span style="font-size:14px">SOSIALISASI BUDIDAYA TANAMAN PADA LAHAN TERBATAS DAN AQUAPNIK</span></h2>
+
+<p><span style="font-size:14px">Penyerahan tanaman dari perwakilan Dinas Pertania Kota Semarang ke Kepala Sekolah SDN Gisikdrono 01 sebagai simbolis</span></p>
+
+<hr /></div>
+</div>
+<!-- END ITEM GALERI --><!-- ITEM GALERI -->
+
+<div class="col-md-12 col-sm-12">
+<div class="testimonial">
+<div class="testi_description">
+<h4><img src="http://sdngisikdrono01.dikdas.semarangkota.go.id/uploads/gallery/media/IMG-20210525-WA0052.jpg" style="height:350px; width:500px" /></h4>
+
+<h2><span style="font-size:14px"><strong>KEGIATAN JEDA SEMESTER KELAS 1 SD NEGERI GISIKDRONO 01</strong></span></h2>
+
+<p><span style="font-size:14px">Pelaksanaan <span style="font-size:12px"><span style="font-size:14px">kegiatan jeda semester kelas 1 di DAMPAK.</span></span></span></p>
+
+<hr /></div>
+</div>
+</div>
+<!-- END ITEM GALERI --><!-- ITEM GALERI -->
+
+<div class="col-md-12 col-sm-12">
+<div class="testimonial">
+<div class="testi_description">
+<h4><img src="http://sdngisikdrono01.dikdas.semarangkota.go.id/uploads/gallery/media/IMG-20210525-WA0020.jpg" style="height:350px; width:500px" /></h4>
+
+<h2><span style="font-size:14px"><strong>KEGIATAN PRAMUKA</strong></span></h2>
+
+<p><span style="font-size:14px">Pelaksanaan <span style="font-size:12px"><span style="font-size:14px">pramuka saat di halaman sekolah</span></span></span></p>
+
+<hr /></div>
+</div>
+</div>
+<!-- END ITEM GALERI --><!-- ITEM GALERI -->
+
+<div class="col-md-12 col-sm-12">
+<div class="testimonial">
+<div class="testi_description">
+<h4><img src="http://sdngisikdrono01.dikdas.semarangkota.go.id/uploads/gallery/media/IMG-20210525-WA0030.jpg" style="height:350px; width:500px" /></h4>
+
+<h2><span style="font-size:14px"><strong>KEGIATAN SENAM SEHAT</strong></span></h2>
+
+<p><span style="font-size:14px">Pelaksanaan <span style="font-size:12px"><span style="font-size:14px">Kegiatan senam dilakukan setiap hari Jumat pagi.</span></span> </span></p>
+
+<hr /></div>
+</div>
+</div>
+<!-- END ITEM GALERI --><!-- ITEM GALERI --></div>
+</div>
+</div>
+</div>
+ */}
+        </>
+    );
+};
+
+export default OldHome;
